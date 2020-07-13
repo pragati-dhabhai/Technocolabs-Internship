ce5f7392edf0d8a9dd67fefcd32ee31165b39b76
pragati-dhabhai <pragatidhabhai@gmail.com>
2020-07-13 18:25:26 +0530

    Update emotion_recognition.py
    Day10

--- Task/emotion_recognition.py
+++ Task/emotion_recognition.py
@@ -9,3 +9,15 @@ from keras.optimizers import Adam
 from keras.regularizers import l2
 from keras.utils import np_utils
 df=pd.read_csv('/content/drive/My Drive/fer2013.csv')
+X_train,train_y,X_test,test_y=[],[],[],[]
+for index, row in df.iterrows():
+    val=row['pixels'].split(" ")
+    try:
+        if 'Training' in row['Usage']:
+           X_train.append(np.array(val,'float32'))
+           train_y.append(row['emotion'])
+        elif 'PublicTest' in row['Usage']:
+           X_test.append(np.array(val,'float32'))
+           test_y.append(row['emotion'])
+    except:
+        print(f"error occured at index :{index} and row:{row}")
